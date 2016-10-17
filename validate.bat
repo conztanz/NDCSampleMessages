e14d671227b37102b9b15fa0e958239f3c2b997b
David Pautet <david.pautet@conztanz.com>
2016-10-17 15:47:33 +0200

    Adding validation and minor fixes

--- validate.bat
+++ validate.bat
@@ -2,4 +2,8 @@
 REM xmllint should be part of the system PATH variable
 xmllint --noout --schema ..\ndc-XMLSchemas\AirShoppingRQ.xsd .\Conztanz\RoundTrip\01_AirshoppingRQ.xml
 xmllint --noout --schema ..\ndc-XMLSchemas\AirShoppingRS.xsd .\Conztanz\RoundTrip\02_AirshoppingRS.xml
+xmllint --noout --schema ..\ndc-XMLSchemas\SeatAvailabilityRQ.xsd .\Conztanz\RoundTrip\03_SeatAvailabilityRQ.xml
+xmllint --noout --schema ..\ndc-XMLSchemas\SeatAvailabilityRS.xsd .\Conztanz\RoundTrip\04_SeatAvailabilityRS.xml
+xmllint --noout --schema ..\ndc-XMLSchemas\OrderCreateRQ.xsd .\Conztanz\RoundTrip\05_OrderCreateRQ.xml
+xmllint --noout --schema ..\ndc-XMLSchemas\OrderViewRS.xsd .\Conztanz\RoundTrip\06_OrderViewRS.xml
 xmllint --noout --schema ..\ndc-XMLSchemas\AirDocIssueRQ.xsd .\Conztanz\RoundTrip\07_AirDocIssueRQ.xml
