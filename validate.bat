8c4df3405e2ac3c10920d00f66601a4a3ad82c45
David Pautet <david.pautet@conztanz.com>
2017-03-13 17:32:08 +0100

    New verbs

--- validate.bat
+++ validate.bat
@@ -4,8 +4,12 @@ xmllint --noout --schema ..\ndc-XMLSchemas\AirShoppingRQ.xsd .\Conztanz\RoundTri
 xmllint --noout --schema ..\ndc-XMLSchemas\AirShoppingRQ.xsd .\Conztanz\RoundTrip\01bis_AirshoppingRQ.xml
 xmllint --noout --schema ..\ndc-XMLSchemas\AirShoppingRS.xsd .\Conztanz\RoundTrip\02_AirshoppingRS.xml
 xmllint --noout --schema ..\ndc-XMLSchemas\SeatAvailabilityRQ.xsd .\Conztanz\RoundTrip\03_SeatAvailabilityRQ.xml
+xmllint --noout --schema ..\ndc-XMLSchemas\SeatAvailabilityRQ.xsd .\Conztanz\RoundTrip\03bis_SeatAvailabilityRQ.xml
 xmllint --noout --schema ..\ndc-XMLSchemas\SeatAvailabilityRS.xsd .\Conztanz\RoundTrip\04_SeatAvailabilityRS.xml
 xmllint --noout --schema ..\ndc-XMLSchemas\OrderCreateRQ.xsd .\Conztanz\RoundTrip\05_OrderCreateRQ.xml
 xmllint --noout --schema ..\ndc-XMLSchemas\OrderViewRS.xsd .\Conztanz\RoundTrip\06_OrderViewRS.xml
 xmllint --noout --schema ..\ndc-XMLSchemas\AirDocIssueRQ.xsd .\Conztanz\RoundTrip\07_AirDocIssueRQ.xml
 xmllint --noout --schema ..\ndc-XMLSchemas\OrderChangeRQ.xsd .\Conztanz\RoundTrip\08_OrderChangeRQ.xml
+xmllint --noout --schema ..\ndc-XMLSchemas\ServiceListRQ.xsd .\Conztanz\RoundTrip\09_ServiceListRQ.xml
+xmllint --noout --schema ..\ndc-XMLSchemas\ServiceListRS.xsd .\Conztanz\RoundTrip\10_ServiceListRS.xml
+xmllint --noout --schema ..\ndc-XMLSchemas\OrderRetrieveRQ.xsd .\Conztanz\RoundTrip\11_OrderRetrieveRQ.xml
